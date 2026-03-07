78e0cb3234dcae33b22d47998ad74cf48f4f88dd
Sreenithy22 <161594371+Sreenithy22@users.noreply.github.com>
2026-03-07 20:01:28 +0530

    Refactor app.py by removing unnecessary git commands
    Removed git commands and cleaned up the code structure.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import streamlit as st
 from transformers import pipeline
 
@@ -11,10 +10,6 @@ sentiment_model = pipeline(
 st.title("🛒 E-Commerce Review Analysis using LLM")
 
 st.write("Enter a customer review and the model will analyze its sentiment.")
-git pull origin main --allow-unrelated-histories
-git add .
-git commit -m "Merged GitHub repo with local project"
-git push -u origin main
 review = st.text_area("Enter Product Review")
 
 if st.button("Analyze Review"):
@@ -31,35 +26,4 @@ if st.button("Analyze Review"):
             st.error(f"😠 Negative Review (Confidence: {score:.2f})")
 
     else:
-=======
-import streamlit as st
-from transformers import pipeline
-
-# Load pretrained sentiment model
-sentiment_model = pipeline(
-    "sentiment-analysis",
-    model="distilbert-base-uncased-finetuned-sst-2-english"
-)
-
-st.title("🛒 E-Commerce Review Analysis using LLM")
-
-st.write("Enter a customer review and the model will analyze its sentiment.")
-
-review = st.text_area("Enter Product Review")
-
-if st.button("Analyze Review") :
-
-    if review.strip() != "":
-        result = sentiment_model(review)
-
-        label = result[0]['label']
-        score = result[0]['score']
-
-        if label == "POSITIVE":
-            st.success(f"😊 Positive Review (Confidence: {score:.2f})")
-        else:
-            st.error(f"😠 Negative Review (Confidence: {score:.2f})")
-
-    else:
->>>>>>> d86f6603683785944a36d5b2d16bc6dc012fb0b2
         st.warning("Please enter a review.")
