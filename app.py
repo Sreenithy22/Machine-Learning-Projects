5254b971087a7cbad000c72253aed7c362bcc14b
Sreenithy <sreenithy22@gmail.com>
2026-03-07 19:49:42 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from transformers import pipeline
+
+# Load pretrained sentiment model
+sentiment_model = pipeline(
+    "sentiment-analysis",
+    model="distilbert-base-uncased-finetuned-sst-2-english"
+)
+
+st.title("🛒 E-Commerce Review Analysis using LLM")
+
+st.write("Enter a customer review and the model will analyze its sentiment.")
+
+review = st.text_area("Enter Product Review")
+
+if st.button("Analyze Review"):
+
+    if review.strip() != "":
+        result = sentiment_model(review)
+
+        label = result[0]['label']
+        score = result[0]['score']
+
+        if label == "POSITIVE":
+            st.success(f"😊 Positive Review (Confidence: {score:.2f})")
+        else:
+            st.error(f"😠 Negative Review (Confidence: {score:.2f})")
+
+    else:
+        st.warning("Please enter a review.")
